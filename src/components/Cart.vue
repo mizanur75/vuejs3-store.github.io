991f0f7b942ac0fd5e2b44a222cb4cac33199747
unknown <mizanurrahman615@gmail.com>
2019-02-18 14:08:43 +0600

    Components Created

---
+++ src/components/Cart.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="col-md-3">
+        <table class="table table-sm">
+        <thead>
+            <tr>
+            <th>#SL</th>
+            <th>Name</th>
+            <th>U/P</th>
+            <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+            <td colspan="2">Total</td>
+            <td>/=</td>
+            </tr>
+        </tfoot>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
